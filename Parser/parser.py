4204a2279a7ff59b094e69f7188e99acba4fa475
ohohcakester <ohohgit@gmail.com>
2015-02-07 20:18:09 +0800

    text parser

--- Parser/parser.py
+++ Parser/parser.py
@@ -1,4 +1,15 @@
+from __future__ import absolute_import
+from __future__ import division, print_function, unicode_literals
+
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+
 import nltk
+from textblob import TextBlob
+import re
 
 def tokenize(s):
 	tokens = s.split(" ")
@@ -13,7 +24,10 @@ def getFreqs(tokens):
 			freqs[token] += 1
 		else:
 			freqs[token] = 1
-	
+	return freqs
+
+def getRevFreqs(tokens):
+	freqs = getFreqs(tokens)
 	return reverseFreqs(freqs)
 
 def reverseFreqs(freqs):
@@ -26,21 +40,95 @@ def reverseFreqs(freqs):
 	for i in range(0, length+1):
 		rev[i] = []
 
-	print(len(rev))
 	for key in freqs:
 		rev[freqs[key]].append(key)
 
 	return rev
 
 
-def main():
-	f = open('sample.txt')
-	s = f.read()
+def freqAnalyse(s):
 	tokens = tokenize(s)
 	freqs = getFreqs(tokens)
 	#print(freqs)
 	for i in range(1, len(freqs)):
-		print str(i) + "\n : " + str(freqs[i]) + '\n'
+		print(str(i) + "\n : " + str(freqs[i]) + '\n')
+
+def getString():
+	f = open('sample3.txt')
+	s = f.read()
+	f.close()
+	return s
+
+def tokenizer(s):
+	tokenizer = colouredtext.SentenceTokenizer()
+	sentences = (tokenizer.segment_text(s))
+	for sentence in sentences:
+		print(sentence)
+		print('\n')
+
+def printSafe(s):
+	#return s
+	s = str(s).encode('ascii', 'ignore')
+	s = s.decode('ascii')
+	print(s)
+
+def niceify(s):
+	s = re.sub("[^\w\s.,\-]", '', s)
+	return s
 
 
-main()
+def getSentences(s):
+	LANGUAGE = "english"
+	SENTENCES_COUNT = 10
+
+	parser = PlaintextParser.from_string(s, Tokenizer(LANGUAGE))
+    #parser = PlaintextParser.from_file("sample3.txt", Tokenizer(LANGUAGE))
+	stemmer = Stemmer(LANGUAGE)
+
+	summarizer = Summarizer(stemmer)
+	summarizer.stop_words = get_stop_words(LANGUAGE)
+
+	sentences = summarizer(parser.document, SENTENCES_COUNT)
+	return sentences
+
+
+
+def getKeywords(s):
+	#s = getString()
+	#printSafe(s)
+
+	blob = TextBlob(s)
+	#printSafe(blob.tags)
+	#print('\n')
+	nounList = blob.noun_phrases
+	freqs = getFreqs(nounList)
+	return freqs.keys()
+	#for i in range(1, len(freqs)):
+	#	printSafe(str(i) + "\n : " + str(freqs[i]) + '\n')
+
+
+	#for noun in nounList:
+	#	printSafe(noun)
+
+
+
+def getResults(s):
+	results = []
+
+	sentences = getSentences(s)
+	for sentence in sentences:
+		strSentence = sentence._text
+		sentenceResult = [strSentence]
+		keywords = getKeywords(strSentence)
+		for keyword in keywords:
+			sentenceResult.append(keyword)
+		results.append(sentenceResult)
+	return results
+
+"""
+results = getResults(getString())
+
+for result in results:
+	print(result)
+	print('\n')
+"""
