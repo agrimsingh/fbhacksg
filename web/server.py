be5ca68f7f42658dbc337a097cd21a945adcc0bd
ohohcakester <ohohgit@gmail.com>
2015-02-07 20:54:22 +0800

    Make web server work with parser.

--- web/server.py
+++ web/server.py
@@ -1,14 +1,34 @@
 from flask import Flask, request
+import speechparser
 app = Flask(__name__)
+
+
+def testFormat(results):
+	sb = []
+	for result in results:
+		sb.append(result[0])
+		sb.append('\n')
+		for i in range(1, len(result)):
+			sb.append(result[i])
+			sb.append(', ')
+		sb.append('\n\n')
+	
+	return ''.join(sb)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+	return 'Hello World!'
 
 @app.route('/summary', methods=['GET', 'POST'])
 def summarize():
-    raw_text = request.form['text']
-    return str(len(raw_text))
+	raw_text = request.form['text']
+	results = speechparser.getResults(raw_text)
+	message = testFormat(results)
+	return message
+
+
+	#return str(len(raw_text))
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
